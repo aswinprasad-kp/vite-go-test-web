4bade23136947e3ce219e95312d1962168b44179
aswinprasad-kp <aswinprasad.kp@silqfi.com>
2026-02-19 15:12:23 +0530

    fix: proxy redirect

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  // Using 127.0.0.1 is often more stable than 'localhost' for Node.js proxies
+  const BACKEND_URL = env.VITE_BE_URL || 'http://127.0.0.1:8080';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        // This catches /api, /auth, /db, and anything else you add in Go
+        '^/(api|auth|db|v1)/.*': {
+          target: BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  }
 })
